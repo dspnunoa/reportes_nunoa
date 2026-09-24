adce7661fa661c8a3cb0b2ef008daa4eab2f414b
cslaregla <cslaregla@uc.cl>
2026-09-24 13:50:45 -0300

    Se actualiza la información hasta el día 23-09-2026.

--- auxiliar.py
+++ auxiliar.py
@@ -115,7 +115,7 @@ lprod = list(df['TIPO DE PROCEDIMIENTO'].unique())
 
 ## Chequeo entradas mal ingresadas ##
 for index, row in df.iterrows():
-    if str(row['TIPO DE PROCEDIMIENTO']) == 'Menor de edad extraviada ':
+    if str(row['TIPO DE PROCEDIMIENTO']) == 'Consumo de Alcohol ':
         print(row['NRO'])
 
 ## Chequeo tipos de procedimientos mal ingresados ##
